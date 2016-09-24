475727914b5b569541ca93a6b304ec3169bf2d23
tgaribal <tgaribal@gmail.com>
2016-09-24 12:30:23 -0700

    Complete drag and initial collision check

--- client/watchout.js
+++ client/watchout.js
@@ -33,15 +33,12 @@ var setPositions = function() {
   .attr('cy', function(d) { return Math.random() * parent.innerHeight * 0.8; });
 };
 
-setInterval(function () {
-  console.log('running');
+setInterval(function () { 
   setPositions(); 
 }, 2000);
 
 
 // create a new circle
-
-//var user = { cx: parent.innerWidth / 2, cy: parent.innerWidth / 2};
 
 var centerXString = d3.select('svg').style('width');
 var centerXNum = Number((centerXString.substring(0, centerXString.length - 2 ) / 2 ));
@@ -50,33 +47,69 @@ var centerYString = d3.select('svg').style('height');
 var centerYNum = Number((centerYString.substring(0, centerYString.length - 2) / 2));
 
 
-
-
-gameBoard.append('rect')
-.classed('user draggable', true).data([1])
-.attr({
-  x: centerXNum,
-  y: centerYNum,
-})
-.style('fill', 'blue')
-.attr('x', function (d) { return d.x; })
-.attr('y', function (d) { return d.y; })
-.call(onDragDrop(dragmove, dropHandler));
-
-var onDragDrop = function (dragHandler, dropHandler) {
+var onDragDrop = function (dragMove) {
   var drag = d3.behavior.drag();
-  drag.on('drag', dragHandler)
-  .on('dragend', dropHandler);
+  drag.on('drag', dragMove)
+  .on('dragend');
   return drag;
-};
-
-var dropHandler = function (d) {
-  // alert('dropped');
 };
 
 var dragmove = function (d) {
   d3.select(this)
-  .attr('x', d.x = d3.event.x)
-  .attr('y', d.y = d3.event.y);
+  .attr('x', d3.event.x - 15)
+  .attr('y', d3.event.y - 15);
 };
+
+
+gameBoard.append('rect')
+.classed('user', true).data([1])
+.attr('x', centerXNum)
+.attr('y', centerYNum)
+.style('fill', 'blue')
+.call(onDragDrop(dragmove));
+
+var collision = false;
+var highScore = highScore || 0;
+var collisionNum = 0;
+
+
+d3.timer(function(elapsed) {
+  var score = Math.floor(elapsed / 100);
+  d3.select('.currentScore').text(score);
+  if (checkCollisions()) {
+    collisionNum++;
+    d3.timer.stop();
+    d3.select('.collisionScore').text(collisionNum);
+    if (score > highScore) {
+      highScore = score;
+      d3.select('.highScore').text(highScore);
+    }
+  }
+}, 10);
+//console.log(t);
+
+//initiate checkCollisions function
+var checkCollisions = function() {
+//query for all circle elements
+  d3.selectAll('circle')
+  .each(function (element) {
+    var xDiff = Math.abs(element.cx - d3.select('rect').attr('x'));
+    var yDiff = Math.abs(element.cy - d3.select('rect').attr('y'));
+    //console.log(element);
+    if (yDiff < 15) {
+      console.log(xDiff)
+      console.log('collision!');
+      return true;
+    }
+  });
+  return false;
+  //check if cx of circle = cx of mouse AND cy of circle = cy of mouse (approximately)
+    //collision = true
+    //otherwise collision is false
+};
+
+//call check collisions every few milliseconds
+
+
+
 
